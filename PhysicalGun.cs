64240777cd9113d7c8f3c6e676128f8977a37c06
TheShaman29 <76856664+TheShaman29@users.noreply.github.com>
2021-01-03 21:56:34 +0100

    Add files via upload

---
+++ PhysicalGun.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicalGun : MonoBehaviour
+{
+    public GameObject ShootingAnchor;
+    public GameObject Projectile;
+    public float ProjectileInitSpeed;
+    void Start(){
+        
+    }
+
+    void Update(){
+        if (Input.GetMouseButtonDown(0)){
+            GameObject projectile;
+            projectile = Instantiate(Projectile, ShootingAnchor.transform.position, ShootingAnchor.transform.rotation);
+            projectile.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.forward * ProjectileInitSpeed);
+            Destroy(projectile, 5);
+        }
+    }
+}
---
+++ RayGun.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayGun : MonoBehaviour
+{
+    public GameObject ShootingAnchor;
+    public float GunRange;
+    void Start(){
+        
+    }
+
+    void Update(){
+        if (Input.GetMouseButtonDown(0)){
+            RaycastHit hit;
+            if (Physics.Raycast(ShootingAnchor.transform.position, ShootingAnchor.transform.TransformDirection(Vector3.forward), out hit, GunRange)){
+                //do sth with hit
+            }
+        }
+    }
+}
